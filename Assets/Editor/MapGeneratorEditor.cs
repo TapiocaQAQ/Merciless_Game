c85fd7790497334e23489ec198517950f2a27c35
JOHN-NB\葉奕均 <fengyu01@ms6.hinet.net>
2022-01-21 16:15:59 +0800

    MapGenerator_ver1

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(MapGenerator)),CanEditMultipleObjects]
+public class MapGeneratorEditor : Editor {
+    public override void OnInspectorGUI() {
+
+        base.OnInspectorGUI();
+        MapGenerator mapGen = (MapGenerator)target;
+
+        
+        DrawDefaultInspector();
+
+        if(GUILayout.Button("Generate")){
+            mapGen.GenerateMap();
+        }
+        
+        
+    }
+}
+
+
