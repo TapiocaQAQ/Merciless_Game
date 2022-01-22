038754de63d57066e58a0247293019327e1bf500
JOHN-NB\葉奕均 <fengyu01@ms6.hinet.net>
2022-01-23 02:02:26 +0800

    MapGeneratorVer3(color map)

--- Assets/Scripts/Terrain_generator/MapGenerator.cs
+++ Assets/Scripts/Terrain_generator/MapGenerator.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+   public enum DrawMode {NoiseMap, ColourMap};
+   public DrawMode drawMode;
    public int mapWidth;
    public int mapHeight;
    public float noiseScale;
 
 
    public int octaves;
-   
+
    [Range(0,1)]
    public float persistance;
    public float lacunarity;
@@ -20,11 +22,29 @@ public class MapGenerator : MonoBehaviour
    public Vector2 offset;
 
    public bool autoUpdate;
+   public TerrainType[] regions; 
+       
    public void GenerateMap(){
        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+       Color[] colourMap = new Color[mapWidth * mapHeight];
+       for(int y = 0; y<mapHeight ; y++){
+           for(int x = 0 ; x<mapWidth ; x++){
+               float currentHeight = noiseMap[x,y];
+               for(int i = 0; i<regions.Length; i++){
+                   if(currentHeight <= regions[i].height){
+                       colourMap[y * mapWidth + x] = regions[i].colour;
+                       break;
+                   }
+               }
+           }
+       }
 
        MapDisplay display = FindObjectOfType<MapDisplay> ();
-       display.DrawNoiseMap(noiseMap);
+       if(drawMode == DrawMode.NoiseMap){
+           display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+       }else if(drawMode == DrawMode.ColourMap){
+           display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+       }
    }
 
    void OnValidate(){
@@ -40,5 +60,12 @@ public class MapGenerator : MonoBehaviour
        if(octaves < 0){
            octaves = 0;
        }
+   }
+   [System.Serializable]
+   public struct TerrainType{
+       public string name;
+       public float height;
+       public Color colour;
+
    }
 }
